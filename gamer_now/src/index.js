87312e7953c3d80a8c154c954e58650be773bb02
zakwern <zakshitmail@gmail.com>
2019-03-03 15:03:27 -0500

    added redux/thunk/firebase

--- gamer_now/src/index.js
+++ gamer_now/src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import authReducer from './store/reducers/authReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(authReducer, 
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig)
+    )
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 
 serviceWorker.unregister();
