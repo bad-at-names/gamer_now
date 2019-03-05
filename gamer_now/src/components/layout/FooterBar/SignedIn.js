dbdb82ef273bb79317c0796414efced111407dc4
zakwern <zakshitmail@gmail.com>
2019-03-04 22:24:22 -0500

    signout working

--- gamer_now/src/components/layout/FooterBar/SignedIn.js
+++ gamer_now/src/components/layout/FooterBar/SignedIn.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../../store/actions/authActions'
 
-const SignedIn = () => {
+const SignedIn = (props) => {
+    props.signOut()
     return (
         <div>
-            <li><Link to='/'>Log Out</Link></li>
+            <li><a onClick={props.signOut}>Log Out</a ></li>
             <li><Link to='/'>Profile</Link></li>
         </div>
     )
 }
 
-export default SignedIn;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedIn);
--- gamer_now/src/store/reducers/authReducer.js
+++ gamer_now/src/store/reducers/authReducer.js
@@ -16,6 +16,9 @@ const authReducer = (state = initState, action) => {
                 ...state,
                 authError: null
             }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success');
+            return state;
         default:
             return state;
     }
