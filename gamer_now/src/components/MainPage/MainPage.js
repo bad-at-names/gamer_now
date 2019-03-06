5537be218265839385fd11357fcc210fef571822
Swornim Barahi <sbara002@plattsburgh.edu>
2019-03-06 15:50:35 -0500

    Fixed an auto logout issue.

--- gamer_now/src/components/MainPage/MainPage.js
+++ gamer_now/src/components/MainPage/MainPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './MainPage.css';
 import FantasyImg from './../../main_assets/fantasyimg.svg';
-import ForumImg from './../../main_assets/forumimg.svg';
+import ForumImg from './../../main_assets/forumimg.png';
 import CoachImg from './../../main_assets/coachimg.svg';
 
 const MainPage = () => {
--- gamer_now/src/components/layout/FooterBar/SignedIn.js
+++ gamer_now/src/components/layout/FooterBar/SignedIn.js
@@ -4,10 +4,9 @@ import { connect } from 'react-redux';
 import { signOut } from '../../../store/actions/authActions'
 
 const SignedIn = (props) => {
-    props.signOut()
     return (
         <div>
-            <li><a onClick={props.signOut}>Log Out</a ></li>
+            <li><button onClick={props.signOut}>Log Out</button ></li>
             <li><Link to='/'>Profile</Link></li>
         </div>
     )
