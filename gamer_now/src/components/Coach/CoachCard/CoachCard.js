70b7802254abe24b76cb77b35d5322dfc983fce1
Eddielo23 <elope007@plattsburgh.edu>
2019-04-09 17:47:12 -0400

    Created coach card component

---
+++ gamer_now/src/components/Coach/CoachCard/CoachCard.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from "react";
+import "./CoachCard.css";
+
+const CoachCard = (props) => {
+
+
+    return (
+        <div className="card">
+            <div className="coach-img">
+                <img src="https://bnetcmsus-a.akamaihd.net/cms/gallery/GOWK7Y2YLJYC1549652245653.png" alt="coach" className="coach-image" />
+            </div>
+            <div className="content">
+                <h2><b>ShadowBurn</b></h2> 
+                <hr></hr>
+                <p>Overwatch Flex DPS Player</p>
+                <div className="booking-button">
+                    <button>Book Coach</button> 
+                </div>
+            </div>
+         
+        </div>
+    );
+}
+
+export default CoachCard;
