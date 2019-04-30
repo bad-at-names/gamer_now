162066bbf26ea4eabc37b8b60ebbba4bedddf4cb
Swornim Barahi <sbara002@plattsburgh.edu>
2019-04-30 17:34:31 -0400

    Added Redirect for the forum post images section.

--- gamer_now/src/components/Forum/Forum.js
+++ gamer_now/src/components/Forum/Forum.js
@@ -1,66 +1,65 @@
 import React, { Component } from "react";
-//import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import NewPostForm from "./NewPost/NewPostForm";
-import Post from "./Post/Post"
+import Post from "./Post/Post";
 import firebase from "firebase";
 
 class Forum extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        };
-    }
-    componentDidMount() {
-        var s = this.state.posts;
-        var db = firebase.firestore();
-        db.collection("posts")
-            .orderBy("title", "asc")
-            .get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(doc => {
-                    s.push(doc.data());
-                });
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    var s = this.state.posts;
+    var db = firebase.firestore();
+    db.collection("posts")
+      .orderBy("title", "asc")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          s.push(doc.data());
+        });
 
-                this.setState({
-                    posts: s
-                });
+        this.setState({
+          posts: s
+        });
 
-                console.log(this.state.posts);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
-    render() {
-        //const { auth } = this.props;
-        //if (!auth.uid) return <Redirect to='/signin' />
-        const { posts } = this.state;
-        const postList = posts.length ? (
-            posts.map(post => {
-                return <Post post={post} key={post.title} />
-            })
-        ) : (
-            <h2>Loading ...</h2>
-        );
+        console.log(this.state.posts);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    //const { auth } = this.props;
+    //if (!auth.uid) return <Redirect to='/signin' />
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return <Post post={post} key={post.title} />;
+      })
+    ) : (
+      <h2>Loading ...</h2>
+    );
 
-        return (
-            <div>
-                {/* <NewPostForm /> */}
-                {/* <Post /> */}
-                {postList}
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Link to="./newpostform">New Post Form</Link>
+        {/* {postList} */}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    //console.log(state);
-    return {
-        auth: state.firebase.auth
-    };
+  //console.log(state);
+  return {
+    auth: state.firebase.auth
+  };
 };
 
 //export default connect(mapStateToProps)(Forum);
--- gamer_now/src/components/Forum/NewPost/NewPostForm.js
+++ gamer_now/src/components/Forum/NewPost/NewPostForm.js
@@ -4,123 +4,123 @@ import firebase from "firebase";
 import "./NewPost.css";
 
 class NewPostForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "",
-            content: "",
-            username: "",
-            password: ""
-           
-        };
-    }
-
-    handleChange = e => {
-        this.setState({
-            [e.target.id]: e.target.value
-        });
-        console.log(this.state);
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      question: "",
+      username: "",
+      password: ""
     };
-    handleSubmit = e => {
-        var success = true;
-        var user = firebase.auth().currentUser;
-        var credentials = firebase.auth.EmailAuthProvider.credential(
-            this.state.username,
-            this.state.password
-        );
-        e.preventDefault();
-        if (user) {
-            user.reauthenticateAndRetrieveDataWithCredential(credentials)
-                .then(() => {
-                    var db = firebase.firestore();
-                    db.collection("posts")
-                        .doc(this.state.username)
-                        .set({
-                            title: this.state.title,
-                            email: this.state.username,
-                            content: this.state.content,
-                        });
-                })
-                .catch(error => {
-                    if (error === null) {
-                        success = false;
-                    }
-                });
-        } else {
-            alert("You need to login first.");
-        }
-        if (success) {
-            this.setState({
-                success: true
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+    console.log(this.state);
+  };
+
+  handleSubmit = e => {
+    var success = true;
+    var user = firebase.auth().currentUser;
+    var credentials = firebase.auth.EmailAuthProvider.credential(
+      this.state.username,
+      this.state.password
+    );
+    e.preventDefault();
+    if (user) {
+      user
+        .reauthenticateAndRetrieveDataWithCredential(credentials)
+        .then(() => {
+          var time = new Date();
+          var timeStamp = time.getTime();
+          var db = firebase.firestore();
+          db.collection("posts")
+            .doc(this.state.username.slice(0, 4) + timeStamp.toString())
+            .set({
+              title: this.state.title,
+              email: this.state.username,
+              question: this.state.question,
+              timestamp: timeStamp,
+              comments: []
             });
-        }
-    };
-
-    render() {
-        if (!this.state.success) {
-            return (
-                <div className="coach-form-super-container">
-                    <form
-                        className="coach-form-container"
-                        onSubmit={this.handleSubmit}
-                    >
-                        <div>
-                            <input
-                                type="text"
-                                name=""
-                                id="Title"
-                                className="cred-inputs"
-                                placeholder="Title"
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                       
-                        <div>
-                            <textarea
-                                name=""
-                                id="Content"
-                                className="dsc-inputs"
-                                placeholder="Content"
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input
-                                type="text"
-                                name=""
-                                id="username"
-                                className="cred-inputs"
-                                placeholder="Username"
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input
-                                type="password"
-                                name=""
-                                id="password"
-                                className="cred-inputs"
-                                placeholder="Password"
-                                onChange={this.handleChange}
-                                required
-                            />
-                        </div>
-                        
-                        <div>
-                            <button className="coach-form-button">
-                                Post
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            );
-        } else {
-            return <Redirect to="./Forum" />;
-        }
+        })
+        .catch(error => {
+          if (error === null) {
+            success = false;
+          }
+        });
+    } else {
+      alert("You need to login first.");
     }
+    if (success) {
+      this.setState({
+        success: true
+      });
+    }
+  };
+
+  render() {
+    var user = firebase.auth().currentUser;
+    if (!this.state.success && user) {
+      return (
+        <div className="coach-form-super-container">
+          <form className="coach-form-container" onSubmit={this.handleSubmit}>
+            <div>
+              <input
+                type="text"
+                name=""
+                id="title"
+                className="cred-inputs"
+                placeholder="Title"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <textarea
+                name=""
+                id="question"
+                className="dsc-inputs"
+                placeholder="question"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name=""
+                id="username"
+                className="cred-inputs"
+                placeholder="Username"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name=""
+                id="password"
+                className="cred-inputs"
+                placeholder="Password"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <button className="coach-form-button">Post</button>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return <Redirect to="./forum" />;
+    }
+  }
 }
 
 export default NewPostForm;
