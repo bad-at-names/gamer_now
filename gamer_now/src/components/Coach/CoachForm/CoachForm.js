44afbc9de11e9992a714e5cfc350fb0ce9230b87
swornimbarahi <sbara002@plattsburgh.edu>
2019-05-15 15:24:19 -0400

    Added Coach comment recognization and display name to all users

--- gamer_now/src/components/Coach/CoachForm/CoachForm.js
+++ gamer_now/src/components/Coach/CoachForm/CoachForm.js
@@ -1,184 +1,198 @@
-import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
-import firebase from "firebase";
-import "./CoachForm.css";
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import firebase from 'firebase';
+import './CoachForm.css';
 
-import G0 from "../../../main_assets/game_logos/game0.png";
-import G1 from "../../../main_assets/game_logos/game1.png";
-import G2 from "../../../main_assets/game_logos/game2.png";
+import G0 from '../../../main_assets/game_logos/game0.png';
+import G1 from '../../../main_assets/game_logos/game1.png';
+import G2 from '../../../main_assets/game_logos/game2.png';
 
 class CoachForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      game: null,
-      dscrptn: "",
-      rate: 0,
-      success: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			game: null,
+			dscrptn: '',
+			rate: 0,
+			success: false
+		};
+	}
 
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-    console.log(this.state);
-  };
+	handleChange = e => {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+		console.log(this.state);
+	};
 
-  handleCheck = e => {
-    var a = e.target.id;
-    this.setState({
-      game: a
-    });
-    console.log(this.state);
-  };
+	handleCheck = e => {
+		var a = e.target.id;
+		this.setState({
+			game: a
+		});
+		console.log(this.state);
+	};
 
-  handleSubmit = e => {
-    var success = true;
-    var user = firebase.auth().currentUser;
-    var credentials = firebase.auth.EmailAuthProvider.credential(
-      this.state.username,
-      this.state.password
-    );
-    e.preventDefault();
-    if (user) {
-      user
-        .reauthenticateAndRetrieveDataWithCredential(credentials)
-        .then(() => {
-          var db = firebase.firestore();
-          db.collection("coaches")
-            .doc(this.state.username)
-            .set({
-              uid: user.uid,
-              email: this.state.email,
-              game: this.state.game,
-              description: this.state.dscrptn,
-              rate: this.state.rate,
-              stars: 0
-            });
-        })
-        .catch(error => {
-          if (error === null) {
-            success = false;
-          }
-        });
-    } else {
-      alert("You need to login first.");
-    }
-    if (success) {
-      this.setState({
-        success: true
-      });
-    }
-  };
+	handleSubmit = e => {
+		var success = true;
+		var user = firebase.auth().currentUser;
+		var credentials = firebase.auth.EmailAuthProvider.credential(
+			this.state.email,
+			this.state.password
+		);
+		e.preventDefault();
+		if (user) {
+			user
+				.reauthenticateAndRetrieveDataWithCredential(credentials)
+				.then(() => {
+					var db = firebase.firestore();
 
-  render() {
-    if (!this.state.success) {
-      return (
-        <div className="coach-form-super-container">
-          <form className="coach-form-container" onSubmit={this.handleSubmit}>
-            <div>
-              <input
-                type="text"
-                name=""
-                id="email"
-                className="cred-inputs"
-                placeholder="Email"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                name=""
-                id="password"
-                className="cred-inputs"
-                placeholder="Password"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-            <div>
-              ${"   "}
-              <input
-                type="number"
-                name=""
-                id="rate"
-                className="rate-input"
-                placeholder="Rate"
-                onChange={this.handleChange}
-                required
-              />
-              {"  "}
-              /hr
-            </div>
-            <div>
-              <textarea
-                name=""
-                id="dscrptn"
-                className="dsc-inputs"
-                placeholder="Description"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-            <div />
-            <div className="check-game">
-              <label className="game-label" htmlFor="dota">
-                <input
-                  type="radio"
-                  value="dota"
-                  name="game"
-                  id="dota"
-                  label="dota"
-                  onChange={this.handleCheck}
-                  className="game"
-                />
-                <img src={G0} className="game-image" alt="" />
-              </label>
-              <label className="game-label" htmlFor="overwatch">
-                <input
-                  type="radio"
-                  value="overwatch"
-                  name="game"
-                  id="overwatch"
-                  label="overwatch"
-                  onChange={this.handleCheck}
-                  className="game"
-                />
-                <img src={G1} className="game-image1" alt="" />
-              </label>
-              <label className="game-label" htmlFor="csgo">
-                <input
-                  type="radio"
-                  value="csgo"
-                  name="game"
-                  id="csgo"
-                  label="csgo"
-                  onChange={this.handleCheck}
-                  className="game"
-                />
-                <img src={G2} className="game-image1" alt="" />
-              </label>
-            </div>
-            <div>
-              <a href="https://connect.stripe.com/oauth/authorize?response_type=code&amp;client_id=ca_E3knZAX3KUUPTvamrcELICFtqgGeY20q&amp;scope=read_write">
-                <span>Connect with Stripe</span>
-              </a>
-            </div>
-            <div>
-              <button className="coach-form-button">Become a Coach</button>
-            </div>
-          </form>
-        </div>
-      );
-    } else {
-      return <Redirect to="./Coach" />;
-    }
-  }
+					db.collection('users')
+						.doc(this.state.email)
+						.get()
+						.then(doc => {
+							var dat = doc.data();
+
+							db.collection('coaches')
+								.doc(this.state.email)
+								.set({
+									name: dat.name,
+									uid: user.uid,
+									email: this.state.email,
+									game: this.state.game,
+									description: this.state.dscrptn,
+									rate: this.state.rate,
+									stars: 0
+								});
+							db.collection('users')
+								.doc(this.state.email)
+								.update({
+									isCoach: true
+								});
+						});
+				})
+				.catch(error => {
+					// if (error === null) {
+					// 	success = false;
+					// }
+				});
+		} else {
+			alert('You need to login first.');
+		}
+		if (success) {
+			this.setState({
+				success: true
+			});
+		}
+	};
+
+	render() {
+		if (!this.state.success) {
+			return (
+				<div className="coach-form-super-container">
+					<form className="coach-form-container" onSubmit={this.handleSubmit}>
+						<div>
+							<input
+								type="text"
+								name=""
+								id="email"
+								className="cred-inputs"
+								placeholder="Email"
+								onChange={this.handleChange}
+								required
+							/>
+						</div>
+						<div>
+							<input
+								type="password"
+								name=""
+								id="password"
+								className="cred-inputs"
+								placeholder="Password"
+								onChange={this.handleChange}
+								required
+							/>
+						</div>
+						<div>
+							${'   '}
+							<input
+								type="number"
+								name=""
+								id="rate"
+								className="rate-input"
+								placeholder="Rate"
+								onChange={this.handleChange}
+								required
+							/>
+							{'  '}
+							/hr
+						</div>
+						<div>
+							<textarea
+								name=""
+								id="dscrptn"
+								className="dsc-inputs"
+								placeholder="Description"
+								onChange={this.handleChange}
+								required
+							/>
+						</div>
+						<div />
+						<div className="check-game">
+							<label className="game-label" htmlFor="dota">
+								<input
+									type="radio"
+									value="dota"
+									name="game"
+									id="dota"
+									label="dota"
+									onChange={this.handleCheck}
+									className="game"
+								/>
+								<img src={G0} className="game-image" alt="" />
+							</label>
+							<label className="game-label" htmlFor="overwatch">
+								<input
+									type="radio"
+									value="overwatch"
+									name="game"
+									id="overwatch"
+									label="overwatch"
+									onChange={this.handleCheck}
+									className="game"
+								/>
+								<img src={G1} className="game-image1" alt="" />
+							</label>
+							<label className="game-label" htmlFor="csgo">
+								<input
+									type="radio"
+									value="csgo"
+									name="game"
+									id="csgo"
+									label="csgo"
+									onChange={this.handleCheck}
+									className="game"
+								/>
+								<img src={G2} className="game-image1" alt="" />
+							</label>
+						</div>
+						<div>
+							<a href="https://connect.stripe.com/oauth/authorize?response_type=code&amp;client_id=ca_E3knZAX3KUUPTvamrcELICFtqgGeY20q&amp;scope=read_write">
+								<span>Connect with Stripe</span>
+							</a>
+						</div>
+						<div>
+							<button className="coach-form-button">Become a Coach</button>
+						</div>
+					</form>
+				</div>
+			);
+		} else {
+			return <Redirect to="./Coach" />;
+		}
+	}
 }
 
 export default CoachForm;
--- gamer_now/src/store/actions/authActions.js
+++ gamer_now/src/store/actions/authActions.js
@@ -1,65 +1,59 @@
 export const signIn = credentials => {
-  return (dispatch, getState, { getFirebase }) => {
-    const firebase = getFirebase();
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase();
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(credentials.email, credentials.password)
-      .then(() => {
-        dispatch({ type: "LOGIN_SUCCESS" });
-      })
-      .catch(err => {
-        dispatch({ type: "LOGIN_ERROR", err });
-      });
-  };
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(credentials.email, credentials.password)
+			.then(() => {
+				dispatch({ type: 'LOGIN_SUCCESS' });
+			})
+			.catch(err => {
+				dispatch({ type: 'LOGIN_ERROR', err });
+			});
+	};
 };
 
 export const signOut = () => {
-  return (dispatch, getState, { getFirebase }) => {
-    const firebase = getFirebase();
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase();
 
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        dispatch({ type: "SIGNOUT_SUCCESS" });
-      });
-  };
+		firebase
+			.auth()
+			.signOut()
+			.then(() => {
+				dispatch({ type: 'SIGNOUT_SUCCESS' });
+			});
+	};
 };
 
 export const signUp = newUser => {
-  return (dispatch, getState, { getFirebase, getFirestore }) => {
-    const firebase = getFirebase();
-    const firestore = getFirestore();
+	return (dispatch, getState, { getFirebase, getFirestore }) => {
+		const firebase = getFirebase();
+		const firestore = getFirestore();
 
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(newUser.email, newUser.password)
-      .then(resp => {
-        console.log(newUser);
-        return (
-          firestore
-            .collection("users")
-            .doc(resp.user.email)
-            .set({
-              email: newUser.email,
-              player: [3380, 3479, 3504, 3577, 3660, 3969],
-              score: 0,
-              avatar: newUser.avatar
-            }) &&
-          firebase
-            .auth()
-            .currentUser()
-            .updateProfile({
-              displayName: newUser.name
-            })
-        );
-      })
-      .then(() => {
-        dispatch({ type: "SIGNUP_SUCCESS" });
-      })
-      .catch(err => {
-        dispatch({ type: "SIGNUP_ERROR", err });
-      });
-  };
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(newUser.email, newUser.password)
+			.then(resp => {
+				console.log(newUser);
+				return firestore
+					.collection('users')
+					.doc(resp.user.email)
+					.set({
+						name: newUser.name,
+						email: newUser.email,
+						player: [3380, 3479, 3504, 3577, 3660, 3969],
+						score: 0,
+						avatar: newUser.avatar,
+						isCoach: false
+					});
+			})
+			.then(() => {
+				dispatch({ type: 'SIGNUP_SUCCESS' });
+			})
+			.catch(err => {
+				dispatch({ type: 'SIGNUP_ERROR', err });
+			});
+	};
 };
